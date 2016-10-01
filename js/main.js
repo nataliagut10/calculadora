05d46ae634a00ac8ae2f4bc764694e80cd5a1923
CJC20 <javierbanguero@hotmail.com>
2016-09-30 19:02:43 -0500

    calculadora division-multiplicacion NATALIA

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+alert("Bienvenido a la Calculadora hecha con javascript")
+var op1 = prompt("Primer Operando");
+var op2 = prompt("Segundo Operando");
+var operador = prompt("Operador (multiplicar,mult,*,dividir,divide,/)");
+var oper = ['multiplicar','mult','*','dividir','divide','/'];
+var veroper = false;
+var resultado;
+var opera;
+
+function multiplicar(p1, p2){
+  resultado = parseFloat(p1)*parseFloat(p2);
+  return resultado;
+};
+
+function dividir(p1, p2){
+  resultado = parseFloat(p1)/parseFloat(p2);
+  return resultado;
+};
+
+if(!isNaN(op1) && !isNaN(op2)){
+  for(var pos in oper){
+    if(oper[pos]===operador){
+      veroper = true
+      if(pos < 3){
+        opera = '*';
+        resultado = multiplicar(op1,op2);
+      }else if(pos > 2 && pos < 6){
+               opera = '/';
+               resultado = dividir(op1,op2);
+            }
+    };
+
+  };
+  if(veroper === true){
+     console.log(op1 + " "+op2+" "+operador+" SI es una operacion Valida!!");
+     alert("Resultado: "+op1 + " " + opera + " " + op2 + " = " + resultado);
+  }else {
+     console.log(op1 + " "+op2+" "+operador+" "+opera+" NO es una operacion Valida!!");
+     alert("No es Valido: "+op1 + " " + operador + " " + op2);
+  }
+}else {
+  console.log(op1 + " "+op2+" "+operador+" "+opera+" NO es una operacion Valida!!");
+  alert("No es Valido: "+op1 + " " + operador + " " + op2);
+}
